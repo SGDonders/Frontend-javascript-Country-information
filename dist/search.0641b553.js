cc613c85f58d95b645fa2a9b6e65201833119cae
S.G. Donders <you@example.com>
2022-11-17 20:43:16 +0100

    Setup project

--- dist/search.0641b553.js
+++ dist/search.0641b553.js
@@ -534,13 +534,15 @@ function hmrAcceptRun(bundle, id) {
 },{}],"bNKaB":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 // Importing axios
+// noinspection JSIgnoredPromiseFromCall
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
-// Fetch data
+// OPDRACHT 1:
 async function fetchData() {
     try {
+        // Fetch data from endpoint
         const axiosResponse = await (0, _axiosDefault.default).get("https://restcountries.com/v2/all");
-        console.log(axiosResponse);
+        //console.log(axiosResponse) // For testing endpoint
         // Sort data
         const countries = axiosResponse.data;
         countries.sort((a, b)=>{
@@ -549,27 +551,27 @@ async function fetchData() {
         // Create document fragment
         const axiosResponseList = document.createDocumentFragment();
         // Iterate through data
-        axiosResponse.data.map((response)=>{
+        axiosResponse.data.map(({ flag , name , population , region  })=>{
             //  Implement flag into fragment
             const elementFlag = document.createElement("img");
             elementFlag.setAttribute("class", "flag");
-            elementFlag.setAttribute("src", response.flag);
+            elementFlag.setAttribute("src", flag);
             axiosResponseList.appendChild(elementFlag);
             // Implement name into fragment
             const elementName = document.createElement("li");
             elementName.setAttribute("class", "name");
-            elementName.textContent = response.name;
+            elementName.textContent = name;
             axiosResponseList.appendChild(elementName);
             // Implement amount of population into fragment
             const elementPopulation = document.createElement("li");
-            elementPopulation.textContent = `Has a population of ${response.population} people.`;
+            elementPopulation.textContent = `Has a population of ${population} people.`;
             axiosResponseList.appendChild(elementPopulation);
             // Implement region into fragment
             const elementRegion = document.createElement("li");
-            elementRegion.textContent = response.region;
+            elementRegion.textContent = region;
             axiosResponseList.appendChild(elementRegion);
             // Switch statement to style color region
-            switch(response.region){
+            switch(region){
                 case "Africa":
                     elementName.style.color = "blue";
                     break;
@@ -591,14 +593,79 @@ async function fetchData() {
             container.appendChild(axiosResponseList);
         });
     } catch (error) {
-        console.error(error);
         const errorMessage = document.getElementById("error-msg");
-        if (error.response.status === 404) errorMessage.textContent = "Page not found | 404";
-        if (error.response.status === 500) errorMessage.textContent = "Internal server error | 500";
-        else errorMessage.textContent = "Page not found";
+        if (error.status === 404) errorMessage.textContent = "Page not found | 404";
+        if (error.status === 500) errorMessage.textContent = "Internal server error | 500";
     }
 }
 fetchData();
+//----------------------------------------------------------------------------------------------------------------------
+// OPDRACHT 2
+const emptyList = document.getElementById("search-ul");
+const errorMessage = document.getElementById("error-search-msg");
+async function fetchSearchData(country) {
+    const axiosSearchResponse = await (0, _axiosDefault.default).get(`https://restcountries.com/v2/name/${country}`);
+    //console.log(axiosSearchResponse) //test output endpoint
+    // Empty list search result and error message
+    emptyList.replaceChildren();
+    errorMessage.replaceChildren();
+    // Fetch data from endpoint
+    axiosSearchResponse.data.map(({ capital , currencies , flag , name , population , subregion , languages  })=>{
+        // Iterate through currencies
+        let coinString;
+        const currenciesArray = currencies.map((coin)=>{
+            return coin.name;
+        });
+        // String builder for currencies
+        if (currencies.length === 1) coinString = `${currenciesArray[0]}`;
+        else coinString = `${currenciesArray[0]} and ${currenciesArray[1]}`;
+        // Iterate trough languages
+        let languagesString;
+        const languagesArray = languages.map((language)=>{
+            return language.name;
+        });
+        // String builder for language
+        if (languagesArray.length === 1) languagesString = `${languagesArray[0]}`;
+        else if (languages.length === 2) languagesString = `${languagesArray[0]} and ${languagesArray[1]}`;
+        else languagesString = `${languagesArray[0]}, ${languagesArray[1]} and ${languagesArray[2]}'s`;
+        // Create document fragment
+        const axiosSearchResponseList = document.createDocumentFragment();
+        // Implement flag into fragment
+        const elementFlag = document.createElement("img");
+        elementFlag.setAttribute("class", "flag");
+        elementFlag.setAttribute("src", flag);
+        axiosSearchResponseList.appendChild(elementFlag);
+        // Implement name into fragment
+        const elementName = document.createElement("li");
+        elementName.textContent = name;
+        axiosSearchResponseList.appendChild(elementName);
+        // Implement String about subregion and population into fragment
+        const elementRegion = document.createElement("li");
+        elementRegion.textContent = `${name} is situated in ${subregion}. 
+                                     It has a population of ${population} people.`;
+        axiosSearchResponseList.appendChild(elementRegion);
+        // Implement capital into fragment
+        const elementCurrencies = document.createElement("li");
+        elementCurrencies.textContent = `The capital is ${capital} and you can pay 
+                                         with ${coinString}`;
+        axiosSearchResponseList.appendChild(elementCurrencies);
+        // Implement language into fragment
+        const elementLanguage = document.createElement("li");
+        elementLanguage.textContent = `They speak ${languagesString}`;
+        axiosSearchResponseList.appendChild(elementLanguage);
+        // Inject fragment into the DOM
+        const container = document.getElementById("search-ul");
+        container.appendChild(axiosSearchResponseList);
+    });
+}
+fetchSearchData();
+// Button function click handler
+let userInput = document.getElementById("searchField");
+const button = document.getElementById("searchButton");
+button.addEventListener("click", ()=>{
+    // noinspection JSIgnoredPromiseFromCall
+    fetchSearchData(userInput.value);
+});
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
@@ -4879,4 +4946,4 @@ exports.default = isAxiosError;
 
 },{"./../utils.js":"5By4s","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["7Aums","bNKaB"], "bNKaB", "parcelRequirecb08")
 
-//# sourceMappingURL=index.0641b553.js.map
+//# sourceMappingURL=search.0641b553.js.map
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 // Importing axios
+// noinspection JSIgnoredPromiseFromCall
+
 import axios from "axios";
 
-// Fetch data
+// OPDRACHT 1:
+
 async function fetchData() {
     try {
+        // Fetch data from endpoint
         const axiosResponse = await axios.get('https://restcountries.com/v2/all');
-        console.log(axiosResponse);
+        //console.log(axiosResponse) // For testing endpoint
 
         // Sort data
         const countries = axiosResponse.data;
@@ -13,35 +17,32 @@ async function fetchData() {
             return a.population - b.population;
         });
 
+
         // Create document fragment
         const axiosResponseList = document.createDocumentFragment();
-
         // Iterate through data
-        axiosResponse.data.map((response)=> {
+        axiosResponse.data.map(({flag, name, population, region}) => {
             //  Implement flag into fragment
             const elementFlag = document.createElement('img');
             elementFlag.setAttribute("class", "flag");
-            elementFlag.setAttribute("src", response.flag);
+            elementFlag.setAttribute("src", flag);
             axiosResponseList.appendChild(elementFlag);
-
             // Implement name into fragment
             const elementName = document.createElement('li');
             elementName.setAttribute("class", "name");
-            elementName.textContent = response.name;
+            elementName.textContent = name;
             axiosResponseList.appendChild(elementName);
-
             // Implement amount of population into fragment
             const elementPopulation = document.createElement('li');
-            elementPopulation.textContent = `Has a population of ${response.population} people.`;
+            elementPopulation.textContent = `Has a population of ${population} people.`;
             axiosResponseList.appendChild(elementPopulation);
-
             // Implement region into fragment
             const elementRegion = document.createElement('li');
-            elementRegion.textContent = response.region;
+            elementRegion.textContent = region;
             axiosResponseList.appendChild(elementRegion);
 
             // Switch statement to style color region
-            switch (response.region) {
+            switch (region) {
                 case "Africa":
                     elementName.style.color = "blue";
                     break;
@@ -58,32 +59,19 @@ async function fetchData() {
                     elementName.style.color = "purple";
                     break;
             }
-
             // Insert document fragment into the DOM
             const container = document.getElementById('data');
             container.appendChild(axiosResponseList);
+        });
 
-       });    } catch( error ) { // Catching errors
-        console.error( error );
+    } catch (error) { // Catching errors
         const errorMessage = document.getElementById("error-msg");
-        if( error.response.status === 404 ){
+        if (error.status === 404) {
             errorMessage.textContent = "Page not found | 404";
         }
-        if ( error.response.status === 500) {
+        if (error.status === 500) {
             errorMessage.textContent = "Internal server error | 500";
-        }else {
-            errorMessage.textContent = "Page not found";
         }
     }
 }
 fetchData();
-
-
-
-
-
-
-
-
-
-
